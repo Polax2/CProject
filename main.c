e42f93033d8119e38e6a642227b617b9278a2b60
Polax2 <276087@student.pwr.edu.pl>
2024-12-27 18:11:26 +0100

    Lets go down

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <signal.h>
 #include <string.h>
 #include "connmgr.h"
 #include "sbuffer.h"
@@ -35,8 +33,31 @@ void logger_process() {
         }
     }
 }
+void main_process(int port, int max_clients) {
+    if (sbuffer_init(&shared_buffer) != SBUFFER_SUCCESS) {
+        fprintf(stderr, "Failed to initialize shared buffer\n");
+        //return EXIT_FAILURE;
+    }
 
-// Main sensor gateway process
+    pthread_t connmgr_tid, storagemgr_tid, datamgr_tid;
+
+    //jakiś struct do przekazania inforrmacji
+
+    // pthread_create(&connmgr_tid, NULL, (void *(*)(void *))connmgr_listen,);
+    pthread_create(&storagemgr_tid, NULL, sensor_db_process, shared_buffer);
+    pthread_create(&connmgr_tid, NULL, sensor_db_process, shared_buffer);
+    pthread_create(&datamgr_tid, NULL, sensor_db_process, shared_buffer);
+
+    pthread_join(connmgr_tid, NULL);
+    pthread_join(storagemgr_tid, NULL);
+
+    sbuffer_free(shared_buffer);
+    pthread_mutex_destroy(&buffer_mutex);
+    pthread_mutex_destroy(&pipe_mutex);
+    pthread_cond_destroy(&buffer_cond);
+
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <port> <max_clients>\n", argv[0]);
@@ -58,40 +79,20 @@ int main(int argc, char *argv[]) {
     } else if (pid == 0) {
         close(log_pipe_fd[1]);  // Close write-end in child
         logger_process();
+        close(log_pipe_fd[0]);
         exit(EXIT_SUCCESS);
+    } else if (pid== 1) {
+        close(log_pipe_fd[0]);  // Close write-end in child
+        main_process(port, max_clients);
+        close(log_pipe_fd[1]);
+        exit(EXIT_SUCCESS);
+
     }
 
-    close(log_pipe_fd[0]);  // Close read-end in parent
 
-    if (sbuffer_init(&shared_buffer) != SBUFFER_SUCCESS) {
-        fprintf(stderr, "Failed to initialize shared buffer\n");
-        return EXIT_FAILURE;
-    }
 
-    pthread_t connmgr_tid, storagemgr_tid;
-
-    struct connmgr_args conn_args = {
-        .port = port,
-        .max_clients = max_clients,
-        .buffer = shared_buffer,
-        .mutex = &buffer_mutex,
-        .cond = &buffer_cond,
-        .log_fd = log_pipe_fd[1],  // Pass write-end to connection manager
-        .pipe_mutex = &pipe_mutex
-    };
-
-    pthread_create(&connmgr_tid, NULL, (void *(*)(void *))connmgr_listen, &conn_args);
-    pthread_create(&storagemgr_tid, NULL, sensor_db_process, shared_buffer);
-
-    pthread_join(connmgr_tid, NULL);
-    pthread_join(storagemgr_tid, NULL);
-
-    sbuffer_free(shared_buffer);
-    pthread_mutex_destroy(&buffer_mutex);
-    pthread_mutex_destroy(&pipe_mutex);
-    pthread_cond_destroy(&buffer_cond);
-
-    close(log_pipe_fd[1]);  // Close write-end after finishing
 
     return EXIT_SUCCESS;
 }
+
+//brakuje zamknięcia dla rodzica, rodzic pid 1 obv,
